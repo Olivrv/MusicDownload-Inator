4ebb69fbb983a5442f9ac6b9d8cd7ffdd07dbe33
Olivrv <60824785+Olivrv@users.noreply.github.com>
2021-08-18 20:19:41 +0200

    Updated input method for spotify link.

--- list.py
+++ list.py
@@ -1,7 +1,9 @@
 from spotinfo import get_playlist_items
 from tools import *
 
-link = "https://open.spotify.com/playlist/1qGJ2P1ajUosy5PFAlMqTr"
+
+link = input('Please insert the link of the spotify playlist.')
+# link = "https://open.spotify.com/playlist/1qGJ2P1ajUosy5PFAlMqTr"
 playlist_link = link[-22:]
 songs = get_playlist_items(playlist_link)
 print("Starting download of: ", *songs)
