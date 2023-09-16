8a2e0aeeb7a66f94ed450c6eeb49d8dadd70823d
Olivrv <60824785+Olivrv@users.noreply.github.com>
2023-09-16 18:32:39 +0200

    Update list-playlist.py

--- list-playlist.py
+++ list-playlist.py
@@ -9,6 +9,6 @@ playlist_link = link[-22:]
 songs = get_hundred_songs(playlist_link, (last_song - offset), offset)
 print("Starting download of: ", *songs)
 for i in songs:
-    download(i, download_location="La chambre 4")
+    download(i, download_location="Playlist_name")
     print("Downloaded", i)
 print("Done.")
